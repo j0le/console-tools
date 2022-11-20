ea5a0490b0539c76551cca6e16949183c640a6e1
Jan Ole Hüser <ols3@hotmail.de>
2022-11-21 00:10:02 +0100

    Print bit flags of console mode
    and use UTF-8 and C++20

--- stty/stty/stty.cpp
+++ stty/stty/stty.cpp
@@ -2,6 +2,99 @@
 
 #include <fmt/core.h>
 #include <nowide/args.hpp>
+#include <string>
+
+//void print_bits(DWORD dw) {
+//	constexpr const int number_of_bits = sizeof(dw) * 8;
+//	for (int i = number_of_bits - 1; i >= 0; --i) {
+//		bool one = 0 != (dw & (1 << i));
+//
+//		fmt::print("{}", one ? "1" : ".");
+//
+//		// space separators
+//		if (i != 0 && (i % 4) == 0)
+//			fmt::print(" ");
+//	}
+//}
+
+#if !defined(UNICODE)
+#error macro UNICODE is not defined
+#endif
+
+#if !defined(_UNICODE)
+#error macro _UNICODE is not defined
+#endif
+
+constexpr const char8_t UTF_8_test_1[] = u8"ü";
+static_assert(sizeof(UTF_8_test_1) == 3);
+static_assert(UTF_8_test_1[0] == static_cast<char8_t>(0xC3u));
+static_assert(UTF_8_test_1[1] == static_cast<char8_t>(0xBCu));
+static_assert(UTF_8_test_1[2] == static_cast<char8_t>(0x0u));
+
+constexpr const char UTF_8_test_2[] = "ü";
+static_assert(sizeof(UTF_8_test_2) == 3);
+static_assert(UTF_8_test_2[0] == static_cast<char>(0xC3u));
+static_assert(UTF_8_test_2[1] == static_cast<char>(0xBCu));
+static_assert(UTF_8_test_2[2] == static_cast<char>(0x0u));
+
+bool get_error_message(DWORD error_code, ::std::string& message) {
+
+	LPWSTR wstr_buffer = nullptr;
+	bool return_value{ true };
+	DWORD number_of_wchars_without_terminating_nul = ::FormatMessageW(
+		FORMAT_MESSAGE_IGNORE_INSERTS   // ignore paremeter "Arguments"
+		| FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_ALLOCATE_BUFFER, // function allocates buffer using
+		                                // LocalAlloc. That means we have to
+		                                // use local free
+		nullptr,                        // lpSource
+		error_code,                     // dwMessageId
+		0, // dwLanguageId. Zero means: Try different languages until message is
+		   // found.
+		reinterpret_cast<LPWSTR>(
+			&wstr_buffer), // We have to cast, because the argument is interpreted
+		                   // differently based on flags
+
+		0,                 // nSize. We don't know the size yet
+		nullptr);
+	if (number_of_wchars_without_terminating_nul == 0) {
+		return_value = false;
+	}
+	else {
+		message = ::nowide::narrow(wstr_buffer,
+			number_of_wchars_without_terminating_nul);
+	}
+
+	if (wstr_buffer != nullptr) {
+		if (LocalFree(wstr_buffer) != nullptr) {
+			return_value = false;
+		}
+	}
+	return return_value;
+}
+
+void PrintMode(std::string_view name, DWORD std_x_handle_id) {
+
+	HANDLE hStd = GetStdHandle(std_x_handle_id);
+	if (hStd == INVALID_HANDLE_VALUE) {
+		fmt::print("{}: invalid\n", name);
+	}
+	else {
+		static_assert(sizeof(HANDLE) == sizeof(uintptr_t));
+
+		fmt::print("{}: {:#x}\n", name, std::bit_cast<uintptr_t>(hStd));
+		DWORD console_output_mode{};
+		if (GetConsoleMode(hStd, &console_output_mode)) {
+			fmt::print("  console mode: {:#0{}b}\n", console_output_mode, sizeof(console_output_mode) * 8 + 2);
+		}
+		else {
+			auto error = GetLastError();
+			std::string message{};
+			get_error_message(error, message);
+			fmt::print("  console mode: error {:#x} \"{}\"", error, message);
+		}
+	}
+}
 
 int main() {
 	UINT acp = GetACP();
@@ -12,5 +105,13 @@ int main() {
 	fmt::print("OEM CP:            {}\n", oem_cp);
 	fmt::print("Console Input CP:  {}\n", console_input_cp);
 	fmt::print("Console Output CP: {}\n", console_output_cp);
+
+	PrintMode("stdin", STD_INPUT_HANDLE);
+	PrintMode("stdout", STD_OUTPUT_HANDLE);
+	PrintMode("stderr", STD_ERROR_HANDLE);
+
+
+	fmt::print("{:#0b}", 0b001000100010001000);
+
 	return 0;
 }
