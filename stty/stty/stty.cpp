191dcac34ff90f65e39616a54699432d0491d066
Jan Ole Hüser <ols3@hotmail.de>
2022-11-21 01:12:24 +0100

    Print Handle type and compare stdout & stderr
    I had an linker error, that the function CompareObjectHandles() couldn't
    be found. The documentation says, it is is kernelbase.lib. But I
    couldn't find kernelbase.lib on my system. So I had to use OneCore.lib
    instead of the default list of libraries (kernel32.lib, ...).
    
    See https://learn.microsoft.com/en-us/windows/win32/apiindex/windows-umbrella-libraries

--- stty/stty/stty.cpp
+++ stty/stty/stty.cpp
@@ -73,10 +73,41 @@ bool get_error_message(DWORD error_code, ::std::string& message) {
 	return return_value;
 }
 
-void PrintMode(std::string_view name, DWORD std_x_handle_id) {
+void PrintFileType(std::string_view leading, HANDLE handle) {
+	auto file_type = GetFileType(handle);
+	switch (file_type) {
+	case FILE_TYPE_CHAR:
+		fmt::print("{}Filetype: FILE_TYPE_CHAR\n", leading);
+		break;
+	case FILE_TYPE_DISK:
+		fmt::print("{}Filetype: FILE_TYPE_DISK\n", leading);
+		break;
+	case FILE_TYPE_PIPE:
+		fmt::print("{}Filetype: FILE_TYPE_PIPE\n", leading);
+		break;
+	case FILE_TYPE_REMOTE:
+		fmt::print("{}Filetype: FILE_TYPE_REMOTE\n", leading);
+		break;
+	case FILE_TYPE_UNKNOWN: {
+		auto error = GetLastError();
+		if (error == NO_ERROR) {
+			fmt::print("{}Filetype: FILE_TYPE_UNKNOWN\n",leading);
+		}
+		else {
+			std::string error_message{};
+			bool got_msg = get_error_message(error, error_message);
+			fmt::print("{0}Filetype: FILE_TYPE_UNKNOWN, error: {1:#x} {1:d} \"{2}\"\n",leading, error, got_msg?error_message:"");
+		}
+		break;
+	}
+	default:
+		fmt::print("{}Filetype: {:#0x} - this is unexpected, undocumented and an error", leading, file_type);
+		break;
+	}
+}
 
-	HANDLE hStd = GetStdHandle(std_x_handle_id);
-	if (hStd == INVALID_HANDLE_VALUE) {
+void PrintMode(std::string_view name, HANDLE hStd) {
+	if (hStd == INVALID_HANDLE_VALUE || hStd == nullptr) {
 		fmt::print("{}: invalid\n", name);
 	}
 	else {
@@ -93,6 +124,8 @@ void PrintMode(std::string_view name, DWORD std_x_handle_id) {
 			get_error_message(error, message);
 			fmt::print("  console mode: error {:#x} \"{}\"", error, message);
 		}
+
+		PrintFileType("  ", hStd);
 	}
 }
 
@@ -106,12 +139,24 @@ int main() {
 	fmt::print("Console Input CP:  {}\n", console_input_cp);
 	fmt::print("Console Output CP: {}\n", console_output_cp);
 
-	PrintMode("stdin", STD_INPUT_HANDLE);
-	PrintMode("stdout", STD_OUTPUT_HANDLE);
-	PrintMode("stderr", STD_ERROR_HANDLE);
+	HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
+	PrintMode("stdin", hStdIn);
 
+	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	PrintMode("stdout", hStdOut);
 
-	fmt::print("{:#0b}", 0b001000100010001000);
+	HANDLE hStdErr = GetStdHandle(STD_ERROR_HANDLE);
+	PrintMode("stderr", hStdErr);
+
+	if (hStdOut != INVALID_HANDLE_VALUE && hStdErr != INVALID_HANDLE_VALUE) {
+		if (hStdOut == hStdErr)
+			fmt::print("The handles for stdout and stderr are equal.\n");
+		if (CompareObjectHandles(hStdOut, hStdErr))
+			fmt::print("The handles for stdout and stderr point to the same kernel object.\n");
+		else
+			fmt::print("The handles for stdout and stderr do not point to the same kernel object.{}\n",
+				hStdOut == hStdErr ? " It seems like, the object they are pointing to is not a kernel object, but some other kind of object." : "");
+	}
 
 	return 0;
 }
