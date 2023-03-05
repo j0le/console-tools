644eb88ced2711005d19a20bb0238e51c3e1600f
Ole <ols3@hotmail.de>
2023-03-05 23:17:20 +0100

    stdin-echo: Set Console Mode of Input
    Set mode so that ReadConsoleW returns one character at a time.
    
    Furthermore Search for Ctrl-C, Ctrl-D, and CR in input.
    
    Search for ^C becuase an Control Event is not generated, becuase the
    mode was changed.
    
    Search for ^D to work like Linux. ^D is EOF by default.
    
    The enter key yields CR. So we replace it with LF, like the option
    "icrlf" of stty on linux, which is enabled by default.

--- stdin-echo/stdin-echo.cpp
+++ stdin-echo/stdin-echo.cpp
@@ -44,6 +44,14 @@ BOOL WINAPI HandleCtrlEvent(
 	}
 }
 
+enum class e_replace_CR_with:uint32_t {
+	CR,
+	CRLF,
+	LF
+};
+
+constexpr const e_replace_CR_with replace_CR_with{ e_replace_CR_with::LF };
+
 int main()
 {
 	_set_fmode(_O_BINARY);
@@ -75,11 +83,28 @@ int main()
 		fmt::print(stderr, "couldn't get standard input/output handles.\n");
 		return 1;
 	}
-	DWORD dummy{};
-	if (not GetConsoleMode(hIn, &dummy)) {
+
+	DWORD console_in_mode{};
+
+	if (not GetConsoleMode(hIn, &console_in_mode)) {
 		fmt::print(stderr, "stdin is not a console.\n");
 		return 1;
 	}
+	struct restore_console_mode {
+		DWORD orig_console_mode{};
+		HANDLE console_handle{};
+		~restore_console_mode() {
+			SetConsoleMode(console_handle, orig_console_mode);
+		}
+	} restore_console_mode_instance{ console_in_mode, hIn };
+
+	if (not SetConsoleMode(hIn, ENABLE_VIRTUAL_TERMINAL_INPUT))
+	{
+		fmt::print(stderr, "SetConsoleMode() failed for stdin.\n");
+		return 1;
+	}
+
+	DWORD dummy{};
 	bool is_stdout_console = GetConsoleMode(hOut, &dummy);
 
 	bool handler_set{ false };
@@ -90,8 +115,57 @@ int main()
 	}
 
 	static constexpr const size_t BUFF_SIZE{ 512 };
-	wchar_t buffer[BUFF_SIZE]{};
 
+	auto print_out = [&](std::wstring_view sv) -> bool {
+		if (sv.size() == 0)
+			return true;
+		if (is_stdout_console) {
+			// StdOut is a console
+			bool success{ false };
+			DWORD absolute_number_of_wide_characters_written{ 0 };
+			DWORD number_of_wchars_written{ 0 };
+			while
+				(
+					absolute_number_of_wide_characters_written < sv.size()
+					&&
+					(
+						success = WriteConsoleW(
+							hOut,
+							&sv[absolute_number_of_wide_characters_written],
+							sv.size() - absolute_number_of_wide_characters_written,
+							&number_of_wchars_written,
+							nullptr)
+						)
+					&&
+					number_of_wchars_written > 0
+					) {
+				absolute_number_of_wide_characters_written += number_of_wchars_written;
+				if (absolute_number_of_wide_characters_written < number_of_wchars_written) {
+					fmt::print(stderr, "Overflow in DWORD.\n");
+					return false;
+				}
+			}
+			if
+				(
+					not success
+					||
+					absolute_number_of_wide_characters_written != sv.size()
+					) {
+				fmt::print(stderr, "Could not write everything to console.\n");
+				return false;
+			}
+		}
+		else {
+			// stdout is not a console
+			const auto str = nowide::narrow(sv);
+			WriteFile(hOut, str.data(), str.size(), &dummy, nullptr);
+			//fmt::print(stdout, "{}", str);
+			//std::fflush(stdout);
+		}
+		return true;
+	};
+
+	wchar_t buffer[BUFF_SIZE]{};
 	do {
 		if (g_ctrl_event_handled) {
 			fmt::print(stderr, "Control-C\n");
@@ -117,50 +191,47 @@ int main()
 			return 1;
 		}
 
-		if (is_stdout_console) {
-			// StdOut is a console
-			bool success{ false };
-			DWORD absolute_number_of_wide_characters_written{ 0 };
-			DWORD number_of_wchars_written{ 0 };
-			while 
-			(
-				absolute_number_of_wide_characters_written < dwWideCharactersRead
-				&&
-				(
-					success = WriteConsoleW(
-						hOut,
-						&buffer[absolute_number_of_wide_characters_written],
-						dwWideCharactersRead - absolute_number_of_wide_characters_written,
-						&number_of_wchars_written,
-						nullptr)
-				)
-				&&
-				number_of_wchars_written > 0
-			) {
-				absolute_number_of_wide_characters_written += number_of_wchars_written;
-				if (absolute_number_of_wide_characters_written < number_of_wchars_written) {
-					fmt::print(stderr, "Overflow in DWORD.\n");
+		std::wstring_view print_later(buffer, dwWideCharactersRead);
+		for (size_t i = 0; i < print_later.size();) {
+			switch(print_later[i])
+			{
+				case wchar_t(0x3):
+				{
+					if (not print_out(print_later.substr(0, i))) { return 1; }
+					fmt::print(stderr, "Control-C");
 					return 1;
 				}
+				case wchar_t(0x4):
+				{
+					if (not print_out(print_later.substr(0, i))) { return 1; }
+					fmt::print(stderr, "Control-D");
+					return 0;
+				}
+
+				case wchar_t(0xd): // ^M, Carriage Return, \r
+				{
+					if (replace_CR_with != e_replace_CR_with::CR) {
+						size_t count = i; // CR not included
+						if (replace_CR_with == e_replace_CR_with::CRLF)
+							count += 1; // include CR
+						if (not print_out(print_later.substr(0, count)))
+							return 1;
+						if (not print_out(std::wstring_view(L"\n")))
+							return 1;
+						print_later = print_later.substr(i + 1);
+						i = 0;
+						continue;
+					}
+					break;
+				}
 			}
-			if
-			(
-				not success
-				||
-				absolute_number_of_wide_characters_written != dwWideCharactersRead
-			) {
-				fmt::print(stderr, "Could not write everything to console.\n");
-				return 1;
-			}
+
+			++i;
 		}
-		else {
-			// stdout is not a console
-			std::wstring_view sv(buffer, dwWideCharactersRead);
-			const auto str = nowide::narrow(sv);
-			WriteFile(hOut, str.data(), str.size(), &dummy, nullptr);
-			//fmt::print(stdout, "{}", str);
-			//std::fflush(stdout);
-		}
+
+		if (not print_out(print_later))
+			return 1;
+
 
 	} while (true);
 }
