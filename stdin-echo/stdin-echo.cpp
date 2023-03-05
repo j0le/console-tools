8913ba646ffa1cc1bb39263f7bdf173cb7eb1e85
Ole <ols3@hotmail.de>
2023-03-05 11:24:09 +0100

    Add new tool "stdin-echo"

---
+++ stdin-echo/stdin-echo.cpp
@@ -0,0 +1,30 @@
+// Program "stdin-echo"
+//
+// This program should read from stdin and print to stdout
+
+#include <iostream>
+
+#if !defined(UNICODE)
+#error macro UNICODE is not defined
+#endif
+
+#if !defined(_UNICODE)
+#error macro _UNICODE is not defined
+#endif
+
+constexpr const char8_t UTF_8_test_1[] = u8"ü";
+static_assert(sizeof(UTF_8_test_1) == 3);
+static_assert(UTF_8_test_1[0] == static_cast<char8_t>(0xC3u));
+static_assert(UTF_8_test_1[1] == static_cast<char8_t>(0xBCu));
+static_assert(UTF_8_test_1[2] == static_cast<char8_t>(0x0u));
+
+constexpr const char UTF_8_test_2[] = "ü";
+static_assert(sizeof(UTF_8_test_2) == 3);
+static_assert(UTF_8_test_2[0] == static_cast<char>(0xC3u));
+static_assert(UTF_8_test_2[1] == static_cast<char>(0xBCu));
+static_assert(UTF_8_test_2[2] == static_cast<char>(0x0u));
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
