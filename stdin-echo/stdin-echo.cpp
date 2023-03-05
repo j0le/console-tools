fc7f2ca9c8091545de48493964bec0d22385f96a
Ole <ols3@hotmail.de>
2023-03-05 16:00:43 +0100

    stdin-echo: Use fmt::print instead of std::cout

--- stdin-echo/stdin-echo.cpp
+++ stdin-echo/stdin-echo.cpp
@@ -2,7 +2,10 @@
 //
 // This program should read from stdin and print to stdout
 
-#include <iostream>
+#include <Windows.h>
+#include <io.h>
+#include <fcntl.h>
+#include <fmt/format.h>
 
 #if !defined(UNICODE)
 #error macro UNICODE is not defined
@@ -26,5 +29,25 @@ static_assert(UTF_8_test_2[2] == static_cast<char>(0x0u));
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	_set_fmode(_O_BINARY);
+	_setmode(_fileno(stdout), _O_BINARY);
+	_setmode(_fileno(stderr), _O_BINARY);
+	_setmode(_fileno(stdin), _O_BINARY);
+
+	if (GetACP() != 65001) {
+		fmt::print(stderr, "The Active Code Page (ACP) for this process is not UTF-8 (65001).\n"
+			"Command line parsing is not supported.\n"
+			"Your version of Windows might be to old, so that the manifest embedded in the executable is not read. "
+			"The manifest specifies, that this executable wants UTF-8 as ACP.\n"
+			"As a workaround you can activate \"Beta: Use Unicode UTF-8 for worldwide language support\":\n"
+			"  - Press Win+R\n"
+			"  - Type \"intl.cpl\"\n"
+			"  - Goto Tab \"Administrative\"\n"
+			"  - Click on \"Change system locale\"\n"
+			"  - Set Checkbox \"Beta: Use Unicode UTF-8 for worldwide language support\"\n"
+			"\n");
+		//PrintUsage(stderr);
+		return 1;
+	}
+	fmt::print(stdout, "hello");
 }
