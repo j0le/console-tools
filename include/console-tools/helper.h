67c2dd7578ec67de4e33f6d4c36327d12574534c
Ole <ols3@hotmail.de>
2023-06-25 13:43:59 +0200

    Convert spaces to tabs in helper.{cpp,h}

--- include/console-tools/helper.h
+++ include/console-tools/helper.h
@@ -24,57 +24,57 @@ using error_or = std::variant<error_t, other>;
 
 
 enum class ConsoleCtrlEvent : DWORD {
-    ctrl_c_event = CTRL_C_EVENT,
-    ctrl_break_event = CTRL_BREAK_EVENT
+	ctrl_c_event = CTRL_C_EVENT,
+	ctrl_break_event = CTRL_BREAK_EVENT
 };
 
 
 template<class uint_type, std::size_t max_number_of_digits>
 constexpr std::optional<uint_type> string_base10_to_integer(const uint_type(&digits_of_max_number)[max_number_of_digits], const std::string_view& str) {
-    static_assert(sizeof(digits_of_max_number) == sizeof(uint_type) * max_number_of_digits);
-    static_assert(std::is_unsigned_v<uint_type>);
+	static_assert(sizeof(digits_of_max_number) == sizeof(uint_type) * max_number_of_digits);
+	static_assert(std::is_unsigned_v<uint_type>);
 
-    if (str.empty() || str.size() > max_number_of_digits)
-        return std::nullopt;
+	if (str.empty() || str.size() > max_number_of_digits)
+		return std::nullopt;
 
 
-    bool must_check_for_max_num = str.size() == max_number_of_digits;
-    unsigned index = 0;
+	bool must_check_for_max_num = str.size() == max_number_of_digits;
+	unsigned index = 0;
 
-    uint_type out_number = 0;
-    for (char c : str) {
-        static_assert('0' < '9');
-        if (c < '0' || c > '9')
-            return std::nullopt;
+	uint_type out_number = 0;
+	for (char c : str) {
+		static_assert('0' < '9');
+		if (c < '0' || c > '9')
+			return std::nullopt;
 
-        uint_type digit = c - '0';
+		uint_type digit = c - '0';
 
-        if (digit == 0 && out_number == 0)
-            continue;
+		if (digit == 0 && out_number == 0)
+			continue;
 
-        if (must_check_for_max_num) {
-            auto max_num_digit = digits_of_max_number[index];
-            ++index;
-            if (digit > max_num_digit)
-                return std::nullopt;
-            if (digit < max_num_digit)
-                must_check_for_max_num = false;
-        }
+		if (must_check_for_max_num) {
+			auto max_num_digit = digits_of_max_number[index];
+			++index;
+			if (digit > max_num_digit)
+				return std::nullopt;
+			if (digit < max_num_digit)
+				must_check_for_max_num = false;
+		}
 
-        out_number *= 10;
-        out_number += digit;
-    }
-    return out_number;
+		out_number *= 10;
+		out_number += digit;
+	}
+	return out_number;
 }
 
 template<class uint_type, std::size_t max_number_of_digits>
 constexpr bool does_array_represent_max_number(const uint_type(&digits)[max_number_of_digits]) {
-    uint_type value{ 0 };
-    for (int i{ 0 }; i < max_number_of_digits; ++i) {
-        value *= 10;
-        value += digits[i];
-    }
-    return value == std::numeric_limits<uint_type>::max();
+	uint_type value{ 0 };
+	for (int i{ 0 }; i < max_number_of_digits; ++i) {
+		value *= 10;
+		value += digits[i];
+	}
+	return value == std::numeric_limits<uint_type>::max();
 }
 
 
@@ -84,23 +84,23 @@ constexpr std::optional<uint> string_to_uint(const std::string_view& str);
 template<>
 constexpr std::optional<std::uint32_t> string_to_uint<uint32_t>(const std::string_view& str) {
 
-    static_assert(sizeof(uint32_t) == 4);
-    // 2^32 - 1  == 4'294'967'295
-    constexpr const uint32_t max_number_digits[10] = { 4,2,9,4,9,6,7,2,9,5 };
-    static_assert(does_array_represent_max_number(max_number_digits));
+	static_assert(sizeof(uint32_t) == 4);
+	// 2^32 - 1  == 4'294'967'295
+	constexpr const uint32_t max_number_digits[10] = { 4,2,9,4,9,6,7,2,9,5 };
+	static_assert(does_array_represent_max_number(max_number_digits));
 
-    return string_base10_to_integer(max_number_digits, str);
+	return string_base10_to_integer(max_number_digits, str);
 }
 
-             
+			 
 template<>
 constexpr std::optional<std::uint64_t> string_to_uint<uint64_t>(const std::string_view& str) {
 
-    // 2^64 - 1 == 18446744073709551615
-    constexpr const uint64_t digits_of_max_number[] = { 1,8,4,4,6,7,4,4,0,7,3,7,0,9,5,5,1,6,1,5 };
-    static_assert(does_array_represent_max_number(digits_of_max_number));
+	// 2^64 - 1 == 18446744073709551615
+	constexpr const uint64_t digits_of_max_number[] = { 1,8,4,4,6,7,4,4,0,7,3,7,0,9,5,5,1,6,1,5 };
+	static_assert(does_array_represent_max_number(digits_of_max_number));
 
-    return string_base10_to_integer(digits_of_max_number, str);
+	return string_base10_to_integer(digits_of_max_number, str);
 }
 
 
@@ -108,88 +108,88 @@ constexpr std::optional<std::uint64_t> string_to_uint<uint64_t>(const std::strin
 
 constexpr std::optional<HANDLE> string_to_HANDLE(const std::string_view& str) {
 
-    static_assert(sizeof(HANDLE) == 4 || sizeof(HANDLE) == 8);
+	static_assert(sizeof(HANDLE) == 4 || sizeof(HANDLE) == 8);
 
-    typedef std::enable_if_t< sizeof(HANDLE) == 4 || sizeof(HANDLE) == 8,
-        std::conditional_t<sizeof(HANDLE) == 4, uint32_t, uint64_t>>
-        uint_HANDLE;
+	typedef std::enable_if_t< sizeof(HANDLE) == 4 || sizeof(HANDLE) == 8,
+		std::conditional_t<sizeof(HANDLE) == 4, uint32_t, uint64_t>>
+		uint_HANDLE;
 
-    std::optional<uint_HANDLE> uint_opt = string_to_uint<uint_HANDLE>(str);
-    if (!uint_opt)
-        return std::nullopt;
-    
-    return std::bit_cast<HANDLE>(*uint_opt);
+	std::optional<uint_HANDLE> uint_opt = string_to_uint<uint_HANDLE>(str);
+	if (!uint_opt)
+		return std::nullopt;
+	
+	return std::bit_cast<HANDLE>(*uint_opt);
 }
 
 
 
 template<class T>
 struct set_and_reset {
-    static_assert(std::is_unsigned_v<T>);
-    T set_all_ones_to_one{ 0u };
-    T set_all_zeros_to_zero{ std::numeric_limits<T>::max() };
+	static_assert(std::is_unsigned_v<T>);
+	T set_all_ones_to_one{ 0u };
+	T set_all_zeros_to_zero{ std::numeric_limits<T>::max() };
 
-    T change(T v) {
-        v |= set_all_ones_to_one;
-        v &= set_all_zeros_to_zero;
-        return v;
-    }
+	T change(T v) {
+		v |= set_all_ones_to_one;
+		v &= set_all_zeros_to_zero;
+		return v;
+	}
 
-    bool unchanging() {
-        constexpr set_and_reset default_value{};
-        return set_all_ones_to_one == default_value.set_all_ones_to_one 
-            && set_all_zeros_to_zero == default_value.set_all_zeros_to_zero;
-    }
+	bool unchanging() {
+		constexpr set_and_reset default_value{};
+		return set_all_ones_to_one == default_value.set_all_ones_to_one 
+			&& set_all_zeros_to_zero == default_value.set_all_zeros_to_zero;
+	}
 
-    std::optional<std::string> to_string() const {
-        constexpr std::size_t bits{ sizeof(T) * 8 };
-        std::string ret(bits, '.');
-        constexpr T one{ 1u };
-        constexpr T zero{ 0u };
-        T set_ones = set_all_ones_to_one;
-        T set_zeros = set_all_zeros_to_zero;
-        for (int i = bits-1; i >= 0; i--) {
-            bool make_one = one == (one & set_ones);
-            bool make_zero = zero == (one & set_zeros);
-            set_ones >>= 1;
-            set_zeros >>= 1;
-            if (make_one && make_zero)
-                return std::nullopt;
-            if (make_one)
-                ret[i] = '1';
-            if (make_zero)
-                ret[i] = '0';
-        }
-        return ret;
-    }
+	std::optional<std::string> to_string() const {
+		constexpr std::size_t bits{ sizeof(T) * 8 };
+		std::string ret(bits, '.');
+		constexpr T one{ 1u };
+		constexpr T zero{ 0u };
+		T set_ones = set_all_ones_to_one;
+		T set_zeros = set_all_zeros_to_zero;
+		for (int i = bits-1; i >= 0; i--) {
+			bool make_one = one == (one & set_ones);
+			bool make_zero = zero == (one & set_zeros);
+			set_ones >>= 1;
+			set_zeros >>= 1;
+			if (make_one && make_zero)
+				return std::nullopt;
+			if (make_one)
+				ret[i] = '1';
+			if (make_zero)
+				ret[i] = '0';
+		}
+		return ret;
+	}
 };
 
 
 template<class T>
 std::optional<set_and_reset<T>> parse_set_and_reset_string(std::string_view str) {
-    static_assert(std::is_unsigned_v<T>);
-    constexpr std::size_t bits{ sizeof(T) * 8 };
-    if (str.length() > bits)
-        return std::nullopt;
+	static_assert(std::is_unsigned_v<T>);
+	constexpr std::size_t bits{ sizeof(T) * 8 };
+	if (str.length() > bits)
+		return std::nullopt;
 
-    constexpr T one{ 1u };
+	constexpr T one{ 1u };
 
-    set_and_reset<T> ret{0u,0u};
-    for (char c : str) {
-        ret.set_all_ones_to_one <<= 1;
-        ret.set_all_zeros_to_zero <<= 1;
-        if (c == '1') {
-            ret.set_all_ones_to_one |= one;
-        }
-        else if (c == '0') {
-            ret.set_all_zeros_to_zero |= one;
-        }
-        else if (c != '.') {
-            return std::nullopt;
-        }
-    }
-    ret.set_all_zeros_to_zero = ~ret.set_all_zeros_to_zero;
-    return ret;
+	set_and_reset<T> ret{0u,0u};
+	for (char c : str) {
+		ret.set_all_ones_to_one <<= 1;
+		ret.set_all_zeros_to_zero <<= 1;
+		if (c == '1') {
+			ret.set_all_ones_to_one |= one;
+		}
+		else if (c == '0') {
+			ret.set_all_zeros_to_zero |= one;
+		}
+		else if (c != '.') {
+			return std::nullopt;
+		}
+	}
+	ret.set_all_zeros_to_zero = ~ret.set_all_zeros_to_zero;
+	return ret;
 }
 
 
